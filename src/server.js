52a5bf2c14482bcc0c52ccdacf7c81fb668e1b0b
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2021-03-29 10:29:57 -0500

    init commit

---
+++ src/server.js
@@ -0,0 +1,61 @@
+import * as sapper from '@sapper/server';
+import { json } from 'body-parser';
+import compression from 'compression';
+import session from 'express-session';
+import helmet from 'helmet';
+import jwt from 'jsonwebtoken';
+import polka from 'polka';
+import sessionFileStore from 'session-file-store';
+import sirv from 'sirv';
+
+const { PORT, NODE_ENV } = process.env;
+const dev = NODE_ENV === 'development';
+
+const FileStore = sessionFileStore(session);
+const expiryDate = new Date(Date.now() + 60 * 60 * 1000) // 1 hour milisegundos
+
+// TODO: implementar:
+// cookie: {
+//   secure: true,
+//   httpOnly: true,
+//   domain: 'example.com',
+//   path: 'foo/bar',
+//   expires: expiryDate
+// }
+
+polka() // You can also use Express
+	.use(
+		json(),
+		session({
+			secret: ['veryimportantsecret', 'notsoimportantsecret', 'highlyprobablysecret'],
+			name: 'sessionId',
+			resave: false,
+			saveUninitialized: false,
+			cookie: {
+				secure: false,
+				maxAge: 31536000,
+				httpOnly: true,
+				expires: expiryDate
+			},
+			store: new FileStore({
+				path: process.env.NOW ? `/tmp/sapper/sessions` : `.sessions`
+			})
+		}))
+	.use(
+		compression({ threshold: 0 }),
+		sirv('static', { dev }),
+		sapper.middleware({
+			session: req => (
+				{
+					user: req.session && req.session.user,
+					// TODO: si user existe sacar data
+					profile: req.session && req.session.user && jwt.decode(req.session.user.data.token)
+				})
+		})
+	)
+	.use(
+		helmet()
+	)
+	.listen(PORT, err => {
+		if (err) console.log('error', err);
+	});
