3ff4f019a6e5046fa9a385bf044dc42e6210b10e
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2021-03-30 18:23:21 -0500

    add User info

--- src/components/shared/ui/Navbar.svelte
+++ src/components/shared/ui/Navbar.svelte
@@ -1,3 +1,21 @@
+<script>
+  import { stores } from "@sapper/app";
+  import Logout24 from "carbon-icons-svelte/lib/Logout24";
+  import User32 from "carbon-icons-svelte/lib/User32";
+
+  const { session } = stores();
+  const handleLogout = async () => {
+    // const result = await logoutUser();
+    // if (result.status === 200) {
+    //   console.log("cerrar sesion");
+    //   $session.user = null;
+    //   goto("/");
+    // } else {
+    //   alertError("Error al cerrar la sesión. Intente nuevamente.");
+    // }
+  };
+</script>
+
 <nav
   class="flex flex-wrap items-center justify-center text-base md:ml-auto md:mr-auto"
 >
@@ -6,18 +24,14 @@
   <a href="/" class="mr-5 hover:text-gray-900">Third Link</a>
   <a href="/" class="mr-5 hover:text-gray-900">Fourth Link</a>
 </nav>
+<div class="flex flex-row items-center space-x-2">
+  <User32 />
+  <p>Sandra Sanchez</p>
+</div>
 <button
-  class="inline-flex items-center px-3 py-1 mt-4 text-base bg-gray-100 border-0 rounded focus:outline-none hover:bg-gray-200 md:mt-0"
-  >Button
-  <svg
-    fill="none"
-    stroke="currentColor"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    stroke-width="2"
-    class="w-4 h-4 ml-1"
-    viewBox="0 0 24 24"
-  >
-    <path d="M5 12h14M12 5l7 7-7 7" />
-  </svg>
+  class="inline-flex items-center px-3 py-1 mt-4 space-x-1 text-base text-gray-600 bg-gray-100 border-0 rounded md:mt-0"
+  on:click={handleLogout}
+>
+  <Logout24 />
+  <p>Salir</p>
 </button>
