b7fbf6380c88b341555290056b12dc567b552ece
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2021-04-01 18:57:00 -0500

    create services admin

--- src/components/admin/services/admin.services.js
+++ src/components/admin/services/admin.services.js
@@ -50,13 +50,34 @@ export const addEmpresa = async (
   }
 }
 
+export const addAuditoria = async (
+  nameAudit,
+  description,
+  empresaId,
+) => {
+  try {
+    const headers = createHeaders();
+    // headers['Authorization'] = `Bearer ${token}`;
+    const url = `${API_URL}/auditorias`;
+    const response = await axios.post(url, {
+      nameAudit,
+      description,
+      empresaId,
+      createdAt: new Date()
+    }, { headers });
+    return response.status;
+  } catch (error) {
+    console.error('addAuditoria:', error);
+    return error;
+  }
+}
+
 export const getEmpresas = async () => {
   try {
     const headers = createHeaders();
     // headers['Authorization'] = `Bearer ${token}`;
     const url = `${API_URL}/empresas?filter[order]=createdAt%20DESC`;
     const { data } = await axios.get(url, { headers });
-    console.log(data);
     return (data.length > 0) ? data : [];
   } catch (error) {
     console.error('getEmpresas:', error);
@@ -68,11 +89,37 @@ export const getEmpresa = async (id) => {
   try {
     const headers = createHeaders();
     // headers['Authorization'] = `Bearer ${token}`;
-    const url = `${API_URL}/empresas/${id}`;
+    const url = `${API_URL}/empresas/${id}?filter[order]=createdAt%20DESC`;
     const { data } = await axios.get(url, { headers });
     return data;
   } catch (error) {
     console.error('getEmpresas:', error);
+    return error;
+  }
+}
+
+export const getEmpresaAuditoria = async (id) => {
+  try {
+    const headers = createHeaders();
+    // headers['Authorization'] = `Bearer ${token}`;
+    const url = `${API_URL}/empresas/${id}/auditorias?filter[order]=createdAt%20DESC`;
+    const { data } = await axios.get(url, { headers });
+    return data;
+  } catch (error) {
+    console.error('getEmpresaAuditoria:', error);
+    return error;
+  }
+}
+
+export const getInfoAuditoria = async (id) => {
+  try {
+    const headers = createHeaders();
+    // headers['Authorization'] = `Bearer ${token}`;
+    const url = `${API_URL}/auditorias/${id}?filter[include][]=empresa`;
+    const { data } = await axios.get(url, { headers });
+    return data;
+  } catch (error) {
+    console.error('getEmpresaAuditoria:', error);
     return error;
   }
 }
